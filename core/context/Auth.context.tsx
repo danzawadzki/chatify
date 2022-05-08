a230c806d20f0ab4061c995ae54234dce88497f7
danzawadzki <hello@danielzawadzki.com>
2022-05-08 17:40:00 +0200

    Added auth.context and wrapped app with it's provider.

---
+++ core/context/Auth.context.tsx
@@ -0,0 +1,49 @@
+import {
+  createContext,
+  Dispatch,
+  FC,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react'
+
+type AuthContextState = {
+  username?: string
+  setUsername: Dispatch<SetStateAction<string | undefined>>
+  logout: () => void
+}
+
+type AuthContextProps = {
+  children: ReactNode
+}
+
+const AuthContext = createContext<AuthContextState | null>(null)
+
+const AuthProvider: FC<AuthContextProps> = ({ children }) => {
+  const [username, setUsername] = useState<string>()
+
+  const logout = () => setUsername(undefined)
+
+  return (
+    <AuthContext.Provider
+      value={{
+        username,
+        setUsername,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within a AuthProvider')
+  }
+  return context
+}
+
+export { AuthProvider, useAuth }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { AuthProvider } from 'core/context/Auth.context'
 import theme from 'core/theme/theme'
 import 'core/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   )
 }
 
